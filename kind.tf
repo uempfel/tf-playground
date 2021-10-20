dee1403b930658e26d7305476808000e955a8044
12Balu34 <bastian.scherber@gmail.com>
2021-10-20 20:00:48 +0200

    update provider

--- kind.tf
+++ kind.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    kind = {
+      source = "unicell/kind"
+      version = "0.0.2-u2"
+    }
+  }
+}
+
 provider "kind" {}
 
 resource "kind_cluster" "default" {
